greearb/linux-ct-6.11

mt7996: Fix setting txpower.
Pulled in a bunch of bits and pieces from MTK and adtran trees.
Setting txpower seems to mostly work now.

Signed-off-by: Ben Greear <greearb@candelatech.com>

drivers/net/wireless/mediatek/mt76/mt7915/mcu.c
@@ -3456,7 +3456,7 @@ int mt7915_mcu_set_txpower_sku(struct mt7915_phy *phy)
 
 	tx_power = mt7915_get_power_bound(phy, hw->conf.power_level);
 	tx_power = mt76_get_rate_power_limits(mphy, mphy->chandef.chan,
-					      &limits_array, tx_power);
+					      &limits_array, NULL, tx_power);
 	mphy->txpower_cur = tx_power;
 
 	for (i = 0, idx = 0; i < ARRAY_SIZE(mt7915_sku_group_len); i++) {

greearb/linux-ct-6.18

drivers/net/wireless/mediatek/mt76/mt7915/mcu.c
@@ -3504,7 +3504,7 @@ int mt7915_mcu_set_txpower_sku(struct mt7915_phy *phy)
 	tx_power = mt76_get_power_bound(mphy, hw->conf.power_level);
 	if (phy->sku_limit_en) {
 		tx_power = mt76_get_rate_power_limits(mphy, mphy->chandef.chan,
-						      &la, tx_power);
+						      &la, NULL, tx_power);
 		mt7915_update_txpower(phy, tx_power);
 	} else {
 		mt7915_update_txpower(phy, tx_power);