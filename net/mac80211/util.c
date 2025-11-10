greearb/linux-ct-5.2

mac80211: Add logging around cac-work logic.
Try to track down bug with ath10k and 6 vap on each of 2 radios.

Signed-off-by: Ben Greear <greearb@candelatech.com>

net/mac80211/util.c
@@ -3449,6 +3449,7 @@ void ieee80211_dfs_cac_cancel(struct ieee80211_local *local)
 		 * by the time it gets it, sdata->wdev.cac_started
 		 * will no longer be true
 		 */
+		sdata_info(sdata, "dfs_cac_candel, canceling dfs-cac-timer-work.\n");
 		cancel_delayed_work(&sdata->dfs_cac_timer_work);
 
 		if (sdata->wdev.cac_started) {

greearb/linux-ct-6.18

net/mac80211/util.c
@@ -3618,6 +3618,7 @@ void ieee80211_dfs_cac_cancel(struct ieee80211_local *local,
 	lockdep_assert_wiphy(local->hw.wiphy);
 
 	list_for_each_entry(sdata, &local->interfaces, list) {
+		sdata_info(sdata, "dfs_cac_cancel, canceling dfs-cac-timer-work.\n");
 		for (link_id = 0; link_id < IEEE80211_MLD_MAX_NUM_LINKS;
 		     link_id++) {
 			link = sdata_dereference(sdata->link[link_id],